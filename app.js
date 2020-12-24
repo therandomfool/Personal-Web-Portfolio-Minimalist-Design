6c512fb67cccf4037cfb2fe161a5b5f5c247f0f9
therandomfool <mddenver@protonmail.com>
2020-12-24 14:40:10 -0800

    Added Contact page navigation functionality

--- app.js
+++ app.js
@@ -1,49 +1,90 @@
+const boxArea = document.querySelector('.box-area');
+const boxArea2 = document.querySelector('.box-area2');
+
+const contactArea = document.querySelector('.contactContainer');
+const me = document.querySelector('.me');
 
 
+// home toggle
+
+function home() {
+
+    const aboutArea = document.querySelector('.tabContainer');
+    const contactArea = document.querySelector('.contactContainer');
+    
+    boxArea.style.display = 'block';
+    boxArea2.style.display = 'none';
+    me.style.display = 'block';
+    contactArea.style.display = 'none';
+    aboutArea.style.display = 'none';
+}
+
+$(function () {
+    $("#nav-contactarea").load("contact.html");
+
+})
 
 
-
-
-// placement of about panel
+// placement of about & contact panel
 
 $(function () {
     $("#nav-viewarea").load("about.html");
+
 });
 
 //  function to switch about panel on/off
-function about(){
-    const navArea = document.querySelector('.tabContainer');
+function about() {
+    const aboutArea = document.querySelector('.tabContainer');
+    const contactArea = document.querySelector('.contactContainer');
     const me = document.querySelector('.me');
 
-    if (navArea.style.display == 'block'){
-        navArea.style.display = 'none';
-        me.style.display ='block'
+    if (aboutArea.style.display == 'block') {
+        contactArea.style.display = 'none';
+        aboutArea.style.display = 'none';
+        me.style.display = 'block'
     } else {
-        navArea.style.display = 'block';
+        aboutArea.style.display = 'block';
         me.style.display = 'none';
+        contactArea.style.display = 'none';
     }
-   
+
+}
+
+// function toggle contact panel
+
+function contact() {
+    const contactArea = document.querySelector('.contactContainer');
+    const aboutArea = document.querySelector('.tabContainer');
+    const me = document.querySelector('.me');
+
+    if (contactArea.style.display == 'block') {
+        contactArea.style.display = 'none';
+        me.style.display = 'block'
+        aboutArea.style.display = 'none';
+    } else {
+        contactArea.style.display = 'block';
+        me.style.display = 'none';
+        aboutArea.style.display = 'none';
+    }
+
 }
 
 // function to switch home button and portfolio button layer
 
-function home() {
-   
-    const boxArea = document.querySelector('.box-area');
-    const boxArea2 = document.querySelector('.box-area2');
+function portfolio() {
+
     const navArea = document.querySelector('.tabContainer');
-    const me = document.querySelector('.me');
 
     if (boxArea.style.display == 'none') {
         boxArea.style.display = 'block';
         boxArea2.style.display = 'none';
         navArea.style.display = 'none';
-        me.style.display ='block'
+        me.style.display = 'block'
     } else {
         boxArea2.style.display = 'block';
         boxArea.style.display = 'none';
         navArea.style.display = 'none';
-        me.style.display ='block'
-    } 
+        me.style.display = 'block'
+    }
 
 }
