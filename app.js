61bb4e47e10d678d7e6ab08f41d59aa37b186469
therandomfool <mddenver@protonmail.com>
2020-12-24 03:35:26 -0800

    added contact html, conected about html

--- app.js
+++ app.js
@@ -15,15 +15,6 @@ function about(){
     const navArea = document.querySelector('.tabContainer');
     const me = document.querySelector('.me');
 
-    // if (me.style.display == 'block'){
-    //     navArea.style.display = 'none';
-    //     // me.style.display ='block'
-    //     console.log(navArea.style.display, me.style.display)
-    // } else {
-    //     navArea.style.display = 'block';
-    //     me.style.display = 'none';
-    //     console.log(navArea.style.display, me.style.display)
-    // }
     if (navArea.style.display == 'block'){
         navArea.style.display = 'none';
         me.style.display ='block'
@@ -41,15 +32,18 @@ function home() {
     const boxArea = document.querySelector('.box-area');
     const boxArea2 = document.querySelector('.box-area2');
     const navArea = document.querySelector('.tabContainer');
+    const me = document.querySelector('.me');
 
     if (boxArea.style.display == 'none') {
         boxArea.style.display = 'block';
         boxArea2.style.display = 'none';
         navArea.style.display = 'none';
+        me.style.display ='block'
     } else {
         boxArea2.style.display = 'block';
         boxArea.style.display = 'none';
         navArea.style.display = 'none';
+        me.style.display ='block'
     } 
 
 }
